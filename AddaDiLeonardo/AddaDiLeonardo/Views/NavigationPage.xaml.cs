365b0432f8ff0a7b713446359e84a8685280c3f5
IISADMINISTRATOR <iis@pc2l05-15>
2020-01-10 11:09:11 +0100

    Aggiornamento progetto Aggiunta delle pagine: - NavigationPage - HomePage

---
+++ AddaDiLeonardo/AddaDiLeonardo/Views/NavigationPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AddaDiLeonardo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPage : TabbedPage
+    {
+        public NavigationPage ()
+        {
+            InitializeComponent();
+
+            this.Children.Add(new )
+        }
+    }
+}
