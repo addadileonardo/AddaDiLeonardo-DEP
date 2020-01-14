1e7e57a5443b51acdc2c7cb3ba8f3b6468911da1
Andrea Troccolo <troccoloandrea01@gmail.com>
2020-01-14 10:39:15 +0100

    Pagina tappa webview

--- AddaDiLeonardo/AddaDiLeonardo/Views/BaseTappa.xaml.cs
+++ AddaDiLeonardo/AddaDiLeonardo/Views/BaseTappa.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Reflection;
 
 namespace AddaDiLeonardo.Views
 {
@@ -15,7 +16,7 @@ namespace AddaDiLeonardo.Views
         public BaseTappa()
         {
             InitializeComponent();
-            imgTappa.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Tappa_ponte.jpg");
+            imgTappa.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Ponte.jpg");
             imgFirstSection0.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Tappa1.adda.jpg");
             indicator0.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Icons.arrow.png");
             indicator1.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Icons.arrow.png");
@@ -23,14 +24,20 @@ namespace AddaDiLeonardo.Views
             indicator3.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Icons.arrow.png");
             indicator4.Source = ImageSource.FromResource("AddaDiLeonardo.Images.Icons.arrow.png");
 
+            string html = default(string);
+            string htmlFileName = "BaseHtml.html";
+            var assembly = typeof(BaseTappa).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.Files.BaseTappa.{htmlFileName}");
+            using (var reader = new StreamReader(stream))
+            {
+               html = reader.ReadToEnd();
+            }
 
 
 
             wbTappa.Source = new HtmlWebViewSource()
             {
-                Html = @"<html><body>
-                            <h1>CAZZO</h1>
-                        </body></html>"
+                Html = html
             };
 
 
