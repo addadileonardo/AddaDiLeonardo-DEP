e888cabbd7d3af78a723fc50589f36a60cb6386f
Andrea Troccolo <troccoloandrea01@gmail.com>
2020-01-16 20:20:53 +0100

    Custom Label

---
+++ AddaDiLeonardo/AddaDiLeonardo.iOS/CustomLabelRender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AddaDiLeonardo.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(AddaDiLeonardo.CustomLabel),typeof(CustomLabelRender))]
+namespace AddaDiLeonardo.iOS
+{
+    public class CustomLabelRender:LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if(Control!=null)
+            {
+                Control.TextAlignment = UITextAlignment.Justified;
+            }
+        }
+    }
+}
