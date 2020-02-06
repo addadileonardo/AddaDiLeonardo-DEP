30a48ee78f398e3c7d3808e38ced177b5138735c
Andrea Troccolo <troccoloandrea01@gmail.com>
2020-02-06 14:39:36 +0100

    Modifiche tappa rocchetta e collapse,

--- AddaDiLeonardo/AddaDiLeonardo/CustomLabelTitle.cs
+++ AddaDiLeonardo/AddaDiLeonardo/CustomLabelTitle.cs
@@ -9,11 +9,10 @@ namespace AddaDiLeonardo
     {
         public CustomLabelTitle()
         {
-            FontFamily = Device.RuntimePlatform == Device.iOS ? "Roboto-SemiBold" : Device.RuntimePlatform == Device.Android ? "Roboto-Regular.ttf#Roboto" : "Assets/Roboto-Regular.ttf#Roboto";
+            FontFamily = Device.RuntimePlatform == Device.iOS ? "Roboto-Regular" : Device.RuntimePlatform == Device.Android ? "Roboto-Regular.ttf#Roboto" : "Assets/Roboto-Regular.ttf#Roboto";
             FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
             LineHeight = 1.10;
             FontAttributes = FontAttributes.Bold;
-            TextColor = Color.Black;
             Margin = new Thickness(0, 10, 0, 0);
         }
     }
